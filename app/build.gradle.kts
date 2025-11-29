58c3f06c46281b64ce352651b6f0993a53807b6b
Eran Kutner <5628151+ekutner@users.noreply.github.com>
2025-11-29 22:49:33 +0200

    Update version to 0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.kutner.cameragpslink"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8"
+        versionCode = 9
+        versionName = "0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
