09b16a16299709d6680c30d4a409eb75f5885116
Eran Kutner <5628151+ekutner@users.noreply.github.com>
2025-12-13 18:27:18 +0200

    Cleanup unnecessary imports

--- app/src/main/java/org/kutner/cameragpslink/NotificationHelper.kt
+++ app/src/main/java/org/kutner/cameragpslink/NotificationHelper.kt
@@ -8,8 +8,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
