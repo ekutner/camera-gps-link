9a882d8aa68c0c12d79f8d24d18f22be5f39034c
Eran Kutner <5628151+ekutner@users.noreply.github.com>
2026-01-11 00:20:05 +0200

    Update version to 3.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.kutner.cameragpslink"
         minSdk = 26
         targetSdk = 36
-        versionCode = 311
-        versionName = "3.11"
+        versionCode = 312
+        versionName = "3.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
